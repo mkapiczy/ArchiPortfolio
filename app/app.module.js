d9c0c1621c0b027e29f1dedfaefd3f536bd39623
Michał Kapiczyński <michal.kapiczynski@atos.net>
2016-06-28 16:20:02 +0200

    Layout responsywny - bez stylowania specjalnego

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,15 @@
-angular.module('archiApp',[])
+angular.module('archiApp', ['ui.bootstrap'])
+
+.controller('NavBarCtrl', ['$scope', function ($scope) {
+
+
+}])
+
+.controller('DropdownCtrl', function ($scope) {
+
+    $scope.items = [
+        "The first choice!",
+        "And another choice for you.",
+        "but wait! A third!"
+    ];
+});
