877c7748458061a0c90cda3e0dc4c2bdafd9c659
Michał Kapiczyński <michal.kapiczynski@atos.net>
2016-06-29 12:20:56 +0200

    [Views] - conact and about view

--- app/app.module.js
+++ app/app.module.js
@@ -1,29 +1,23 @@
 angular.module('archiApp', ['ui.bootstrap', 'ui.router'])
 
 .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/projects');
 
     $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: 'app/components/home/homeView.html'
+        .state('about', {
+            url: '/about',
+            templateUrl: 'app/components/about/aboutView.html'
         })
         .state('projects', {
             url: '/projects',
             templateUrl: 'app/components/projects/projectsView.html'
         })
-}])
+        .state('contact', {
+        url: '/contact',
+        templateUrl: 'app/components/contact/contactView.html'
+    })
+}]);
 
-.controller('NavBarCtrl', ['$scope', function ($scope) {
 
 
-}])
 
-.controller('DropdownCtrl', function ($scope) {
-
-    $scope.items = [
-        "The first choice!",
-        "And another choice for you.",
-        "but wait! A third!"
-    ];
-});
