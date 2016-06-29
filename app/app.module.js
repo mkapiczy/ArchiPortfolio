1493f99a459aa449695577765c6e28b82dbe66e8
Michał Kapiczyński <michal.kapiczynski@atos.net>
2016-06-29 11:42:33 +0200

    Routing - basic

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,18 @@
-angular.module('archiApp', ['ui.bootstrap'])
+angular.module('archiApp', ['ui.bootstrap', 'ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'app/components/home/homeView.html'
+        })
+        .state('projects', {
+            url: '/projects',
+            templateUrl: 'app/components/projects/projectsView.html'
+        })
+}])
 
 .controller('NavBarCtrl', ['$scope', function ($scope) {
 
